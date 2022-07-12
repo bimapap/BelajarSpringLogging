9554cb6b30b53c2b1b30417a3e5e92dda8329214
Bima Prasetya Adi Pratama <bimapap@gmail.com>
2022-07-12 19:52:47 +0700

    first

---
+++ belajar-spring-logging/src/test/java/bimapap/belajarspringlogging/BelajarSpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package bimapap.belajarspringlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
